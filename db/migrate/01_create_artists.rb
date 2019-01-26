e03281852e287024af93e2455b43fa3b2723bae6
Elyse Burmester <burmesterelyse@gmail.com>
2019-01-26 19:57:58 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -5,6 +5,20 @@ class CreateArtists < ActiveRecord::Migration
   def down
   end
   
+  def create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS artists (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      genre TEXT,
+      age INTEGER,
+      hometown TEXT
+      )
+    SQL
+     
+    ActiveRecord::Base.connection.execute(sql)
+  end
+
   def change
     create_table :artists do |t|
       t.string :name
